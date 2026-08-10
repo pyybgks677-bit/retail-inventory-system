9952c9701e675fd842c81faebd167dc17f69bb96
Bruno Henrique Rodrigues Casarim <bcasarim@hotmail.com>
2026-08-10 11:54:53 +0100

    Update db_config.py

--- db_config.py
+++ db_config.py
@@ -1,9 +1,11 @@
+import os
 import mysql.connector
+
 
 def get_db_connection():
     return mysql.connector.connect(
-        host="127.0.0.1",
-        user="root",
-        password="toor",
-        database="inventory_db"
+        host=os.getenv("DB_HOST", "127.0.0.1"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        database=os.getenv("DB_NAME", "inventory_db")
     )
